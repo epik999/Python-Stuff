f70455fb8e88da7a79e5695154b75f1018d9324f
epik999 <95705410+epik999@users.noreply.github.com>
2021-12-07 17:28:49 +0000

    Add files via upload

---
+++ Arrays.py
@@ -0,0 +1,13 @@
+#arrays
+import tkinter
+import random
+import time
+from typing import Text
+wordlist = ('Welcome', 'Python', 'Monopoly', 'Coding')
+randword = random.choice(wordlist)
+window = tkinter.Tk()
+window.geometry('300x300')
+window.title('Words')
+label = tkinter.Label(window, text = (randword))
+label.place(relx=0.5, rely=0.5, anchor='center')
+window.mainloop()

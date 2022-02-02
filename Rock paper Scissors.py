3f0c3831ccd06342cad998ece9e887baea01b8e0
epik999 <95705410+epik999@users.noreply.github.com>
2022-02-02 11:47:28 +0000

    Add files via upload

--- Rock paper Scissors.py
+++ Rock paper Scissors.py
@@ -1,27 +1,21 @@
 # Rock paper Scissors
-
-i = True
-
-# modules
 import random
 
-
 # function to start game
-
 def rps() :
 
-    start = input('Would you like to start? ( "Y" or "N" ) ')
+    start = input('Would you like to Play? ( Y or N ) ')
     if start.upper() == "Y":
 
         # Possible Choices
         Choice = ('ROCK', 'PAPER', 'SCISSORS')
-    
+
         # Player makes choice
         PlayerChosen = input('Rock, Paper or Scissors : ')
-    
+
         # Computer makes choice
         Computerchoice = random.choice(Choice)
-    
+
         # draw
         if PlayerChosen.upper() == Computerchoice :
             print('You Drew')
@@ -35,17 +29,8 @@ def rps() :
         # loss 
         else :
             print('You Lost')
-    
+
     else:
         quit()
 
-while i == True :
-    rps()
-
-# repeating
-
-PlayAgain = input('Would you like to play again? ( "Y" or "N" ) : ')
-if PlayAgain.upper() == 'Y' :
-    i == True
-else :
-    i == False
+rps()

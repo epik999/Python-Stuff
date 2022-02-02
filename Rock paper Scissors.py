b60622898fa9589d759b71a167cde7092f93e841
epik999 <95705410+epik999@users.noreply.github.com>
2022-02-02 11:45:47 +0000

    Add files via upload

--- Rock paper Scissors.py
+++ Rock paper Scissors.py
@@ -1,35 +1,51 @@
 # Rock paper Scissors
 
+i = True
+
+# modules
 import random
 
-# Possible Choices
-Choice = ('ROCK', 'PAPER', 'SCISSORS')
-
-# Player makes choice
-PlayerChosen = input('Rock, Paper or Scissors : ')
-
-# Computer makes choice
-
-Computerchoice = random.choice(Choice)
 
 # function to start game
 
 def rps() :
-# draw
-    if PlayerChosen.upper() == Computerchoice :
-        print('You Drew')
-# win
 
-    elif PlayerChosen.upper() == 'ROCK' and Computerchoice == 'SCISSORS':
-        print('You Win')
-    elif PlayerChosen.upper() == 'PAPER' and Computerchoice == 'ROCK' :
-      print('You Win')
-    elif PlayerChosen.upper() == 'SCISSORS' and Computerchoice == 'PAPER' :
-        print('You Win')
+    start = input('Would you like to start? ( "Y" or "N" ) ')
+    if start.upper() == "Y":
 
-# loss 
-    else :
-        print('You Lost')
+        # Possible Choices
+        Choice = ('ROCK', 'PAPER', 'SCISSORS')
+    
+        # Player makes choice
+        PlayerChosen = input('Rock, Paper or Scissors : ')
+    
+        # Computer makes choice
+        Computerchoice = random.choice(Choice)
+    
+        # draw
+        if PlayerChosen.upper() == Computerchoice :
+            print('You Drew')
+        # win
+        elif PlayerChosen.upper() == 'ROCK' and Computerchoice == 'SCISSORS':
+            print('You Win')
+        elif PlayerChosen.upper() == 'PAPER' and Computerchoice == 'ROCK' :
+          print('You Win')
+        elif PlayerChosen.upper() == 'SCISSORS' and Computerchoice == 'PAPER' :
+            print('You Win')
+        # loss 
+        else :
+            print('You Lost')
+    
+    else:
+        quit()
 
+while i == True :
+    rps()
 
-rps() 
+# repeating
+
+PlayAgain = input('Would you like to play again? ( "Y" or "N" ) : ')
+if PlayAgain.upper() == 'Y' :
+    i == True
+else :
+    i == False
